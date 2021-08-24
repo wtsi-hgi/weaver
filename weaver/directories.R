afcb18514008a552b385c89ffc08a8f513fe0d52
michael-grace <michael.dp.grace@outlook.com>
2021-08-24 09:16:51 +0100

    fix issue where group/pi combo may not have directory data

--- weaver/directories.R
+++ weaver/directories.R
@@ -22,6 +22,7 @@ getFileUsage <- function(directory_id_filter, connection) {
     filetypes <- tbl(connection, "filetype")  %>% 
         select(c("filetype_id", "filetype_name"))
     
+    # Ask the `file_size` DB table for its information for a particular `directory_id`
     file_usage <- tbl(connection, "file_size")  %>% 
         filter(`directory_id` == directory_id_filter)  %>% 
         select(c("filetype_id", "size"))  %>% 
@@ -29,20 +30,27 @@ getFileUsage <- function(directory_id_filter, connection) {
         mutate(`size` = round(`size`, digits = 2))  %>% 
         collect()
 
+    # We're going to format this into a string, separating the filetype_name and size by ": ", and each row by "<br>"
     return(paste(file_usage$filetype_name, file_usage$size, sep=": ", collapse="<br>"))
 
 }
 
 getDirectories <- function(connection, group_id_filter, volume_id_filter) {
+    # Ask the `directory` DB table for its information by PI and Group
     directories <- tbl(connection, "directory")  %>% 
     filter(`group_id` == group_id_filter)  %>% 
     filter(`volume_id` == volume_id_filter)  %>% 
     select(c("directory_id", "project_name", "directory_path", "num_files", "size", "last_modified"))  %>% 
-    collect() %>% 
-    rowwise() %>% 
-    mutate(
-        "filetypes" = getFileUsage(`directory_id`, connection)
-    )
+    collect()
+
+    # If we've got data, we want to add in all the filetype usages (getFileUsage)
+    # otherwise, we just need to create the column, but leave it empty
+    if (nrow(directories) != 0) {
+        directories <- directories %>% rowwise()  %>% 
+        mutate("filetypes" = getFileUsage(`directory_id`, connection))
+    } else {
+        directories <- directories %>% mutate("filetypes" = "")
+    }
 
     return(directories)
 }
